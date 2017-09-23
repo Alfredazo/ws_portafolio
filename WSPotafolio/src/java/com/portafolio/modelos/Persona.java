6258d5bfd21281dd790de18def11cfb479ecc15e
Alfredazo <andrade.aedo@gmail.com>
2017-09-23 16:05:10 -0300

    Agregue una clase Persona

---
+++ WSPotafolio/src/java/com/portafolio/modelos/Persona.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portafolio.modelos;
+
+/**
+ *
+ * @author Alfredazo
+ */
+public class Persona {
+    
+}
